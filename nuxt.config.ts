d827e7c18bbd3a7ec6a8b133aaa9236ff0c41436
Kunniii <57139436+Kunniii@users.noreply.github.com>
2024-01-13 11:43:02 +0700

    asdf`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from "path";
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  alias: {
+    "@": resolve(__dirname, "/"),
+  },
+  modules: ["@nuxtjs/tailwindcss"],
+
+  ignore: [
+    "**/*.stories.{js,cts,mts,ts,jsx,tsx}",
+    "**/*.{spec,test}.{js,cts,mts,ts,jsx,tsx}",
+    "**/*.d.{cts,mts,ts}",
+    "**/.{pnpm-store,vercel,netlify,output,git,cache,data}",
+    ".nuxt/analyze",
+    ".nuxt",
+    "**/-*.*",
+    "node_modules/",
+  ],
+});
